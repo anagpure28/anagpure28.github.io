19e42f91b6e817ce447ede33909faf8790ca02dc
Aniket Nagpure <aniketnagpure28@gmail.com>
2023-05-23 01:05:54 +0530

    id's

--- JS/script.js
+++ JS/script.js
@@ -2,3 +2,10 @@ const header = document.querySelector("header");
 window.addEventListener("scroll", function() {
     header.classList.toggle("sticky", window.scrollY > 100)
 })
+
+
+// *=============================== github calender ==================================
+GitHubCalendar(".calendar", "anagpure28");
+
+// or enable responsive functionality:
+GitHubCalendar(".calendar", "anagpure28", { responsive: true });
