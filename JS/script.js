170463fad5e7b76fa7bc31fb9b83bdaa1c6d138e
anagpure28 <aniketnagpureofficial@gmail.com>
2023-07-15 15:52:16 +0530

    Updated

--- JS/script.js
+++ JS/script.js
@@ -1,5 +1,4 @@
 
-
 let menu=document.getElementById("menu-icon");
 let navbar=document.querySelector(".navbar");
 let m=document.querySelector(".main-body")
@@ -24,20 +23,5 @@ menu.addEventListener('click',()=>{
 });
 
 
-GitHubCalendar(".calendar", "anagpure28");
-GitHubCalendar(".calendar", "anagpure28", { responsive: true });
-
-
-function resume(){
-    window.open("https://drive.google.com/file/d/1GcYbXrLwf2yxK4e9sVYqd4Cc1itBnLP3/view?usp=sharing")
-}
-
-// let resume1 = document.getElementById("resume-button-1").addEventListener("click", NewTab)
-// let resume2 = document.getElementById("resume-button-2").addEventListener("click", NewTab)
-
-// function NewTab() {
-//     window.open(
-//         "https://drive.google.com/file/d/1rO5-y7PLzKe6JJVbWWduynCAHPkk_g1J/view?usp=drive_link",
-//         "_blank"
-//     );
-// }
+GitHubCalendar(".calendar", "anagpure");
+GitHubCalendar(".calendar", "anagpure", { responsive: true });
